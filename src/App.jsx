58c010e2f95121ecd2ebaec1ded493e803a79e2f
ranaadeel15911 <ra0511083@gmail.com>
2025-06-06 19:59:05 +0500

    ajf

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+<h1>Make Vite to PWA</h1>
+<ul>
+  <li>
+    (1) Install the Vite PWA Plugin <br /> npm install vite-plugin-pwa --save-dev <br />
+
+  </li>
+</ul>
+    </>
+  )
+}
+
+export default App
